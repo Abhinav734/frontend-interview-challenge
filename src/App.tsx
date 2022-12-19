78b698b3304e97f447a3fbe4eb94d9096bee9b18
Abhinav <shravanjukhoop@gmail.com>
2022-12-19 21:18:34 +0400

    add api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 
-import styled from "styled-component";
+import { useEffect } from "react";
+import styled from "styled-components";
 export const App = () => {
 
-  const StyledButton = styled.button`
-  background-color: black;
-  font-size: 32px;
-  color: white;
-`;
-  return <>
-    <div><div>Meetings</div><div>Filter</div></div>
-  </>;
+  useEffect(()=>{
+
+    fetch("http://localhost:8080/data").then((response) => response.json())
+    .then((data) => {
+       console.log(data);
+
+    })
+    .catch((err) => {
+       console.log(err.message);
+    });
+  },[])
+
+  const Contents = styled.div`
+  background-color: red;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  `;
+  
+  const Container = styled.div`
+  
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  `;
+  return <Container>
+    <Contents><div>Meetings</div><div><select>
+
+<option value="fruit">Fruit</option>
+
+<option value="vegetable">Vegetable</option>
+
+<option value="meat">Meat</option>
+
+</select></div></Contents>
+<Contents>
+
+<div>table</div>
+</Contents>
+  </Container>;
 };
