9815c703745dcebfe8933a66c2b10fd3ac8f67bc
Abhinav <shravanjukhoop@gmail.com>
2022-12-21 21:47:15 +0400

    fix cors error on api

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const spawn = require('child_process').spawn;
+
 const data = require('./data.json');
+const express = require('express');
+const port = 8080
+const app = express();
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-const requestListener = (req, res) => {
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.end(JSON.stringify(data));
-};
+app.use(cors(corsOptions))
 
-const server = http.createServer(requestListener);
-server.listen(8080, () => {
-  console.log('Server started; please run `react-scripts start` to start the client.');
-});
+app.get('/data', (req, res) => {
+  res.send(JSON.stringify(data))
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
